c2684ff7aba81cdc977556a1000a26797eda4cf4
Joe Heitkamp <noetic97@gmail.com>
2017-09-06 17:04:40 -0600

    add knexfile paths for dev, testing, and production

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const db = require('knex')(configuration);
 const express = require('express');
 const path = require('path');
-const cors = require('express-cors');
 const bodyParser = require('body-parser');
 
-const environment = process.env.NODE_ENV || 'development';
 const app = express();
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, './build')));
