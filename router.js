c22aa3b349e22115bd16fe5cbdcd27566f4daf13
Joe Heitkamp <noetic97@gmail.com>
2017-09-08 13:09:03 -0600

    add quiz controller

--- router.js
+++ router.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const folder = require('./controllers/folderController');
+const quiz = require('./controllers/quizController');
 
 const router = express.Router();
 
 // Folder
 router.get('/folders', folder.index);
+
+// Quiz
+router.get('/quizzes', quiz.index);
 
 
 module.exports = router;
