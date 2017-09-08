616a2ead3892f01519be8394503fb75f94f9f518
Joe Heitkamp <noetic97@gmail.com>
2017-09-08 12:11:45 -0600

    Fix seed files

--- db/seeds/dev/initial_quiz.js
+++ db/seeds/dev/initial_quiz.js
@@ -15,23 +15,23 @@ exports.seed = (knex, Promise) => {
         .then((teacher) => {
           return knex('folder').insert({
             name: 'Pop Quiz',
-            teacher_id: teacher.id,
-          });
-        }, '*')
+            teacher_id: teacher[0].id,
+          }, '*');
+        })
         .then((folder) => {
           return knex('quiz').insert({
             name: 'First Quiz',
-            folder_id: folder.id,
-            teacher_id: folder.teacher_id,
+            folder_id: folder[0].id,
+            teacher_id: folder[0].teacher_id,
             subject: 'Javascript',
             type: 'Pop Quiz',
-          });
-        }, '*')
+          }, '*');
+        })
         .then((quiz) => {
           return Promise.all([
             knex('question').insert({
               question_text: 'What is your favorite color?',
-              quiz_id: quiz.id,
+              quiz_id: quiz[0].id,
               subject: 'Jquery',
               question_type: 'multiple choice',
               difficulty: 1,
@@ -40,19 +40,19 @@ exports.seed = (knex, Promise) => {
                 return Promise.all([
                   knex('answer').insert({
                     answer_text: 'Blue',
-                    question_id: answer.id,
+                    question_id: answer[0].id,
                   }),
                   knex('answer').insert({
                     answer_text: 'Red',
-                    question_id: answer.id,
+                    question_id: answer[0].id,
                   }),
                   knex('answer').insert({
                     answer_text: 'Yellow',
-                    question_id: answer.id,
+                    question_id: answer[0].id,
                   }),
                   knex('answer').insert({
                     answer_text: 'Chartreuse',
-                    question_id: answer.id,
+                    question_id: answer[0].id,
                     points: 1,
                     correct: true,
                   }),
@@ -60,7 +60,7 @@ exports.seed = (knex, Promise) => {
               }),
             knex('question').insert({
               question_text: 'What does the Array prototype map return?',
-              quiz_id: quiz.id,
+              quiz_id: quiz[0].id,
               subject: 'Arrays',
               question_type: 'true/false',
               difficulty: 3,
@@ -69,11 +69,11 @@ exports.seed = (knex, Promise) => {
                 return Promise.all([
                   knex('answer').insert({
                     answer_text: 'A string',
-                    question_id: answer.id,
+                    question_id: answer[0].id,
                   }),
                   knex('answer').insert({
                     answer_text: 'An array',
-                    question_id: answer.id,
+                    question_id: answer[0].id,
                     points: 1,
                     correct: true,
                   }),
@@ -81,7 +81,7 @@ exports.seed = (knex, Promise) => {
               }),
             knex('question').insert({
               question_text: 'Which of the following are array prototypes?',
-              quiz_id: quiz.id,
+              quiz_id: quiz[0].id,
               subject: 'Arrays',
               question_type: 'multiple choice-multiple answer',
               difficulty: 7,
@@ -90,37 +90,37 @@ exports.seed = (knex, Promise) => {
                 return Promise.all([
                   knex('answer').insert({
                     answer_text: 'blamp',
-                    question_id: answer.id,
+                    question_id: answer[0].id,
                   }),
                   knex('answer').insert({
                     answer_text: 'map',
-                    question_id: answer.id,
+                    question_id: answer[0].id,
                     points: 1,
                     correct: true,
                   }),
                   knex('answer').insert({
                     answer_text: 'reduce',
-                    question_id: answer.id,
+                    question_id: answer[0].id,
                     points: 1,
                     correct: true,
                   }),
                   knex('answer').insert({
                     answer_text: 'split',
-                    question_id: answer.id,
+                    question_id: answer[0].id,
                   }),
                   knex('answer').insert({
                     answer_text: 'Chartreuse',
-                    question_id: answer.id,
+                    question_id: answer[0].id,
                   }),
                   knex('answer').insert({
                     answer_text: 'find',
-                    question_id: answer.id,
+                    question_id: answer[0].id,
                     points: 1,
                     correct: true,
                   }),
                   knex('answer').insert({
                     answer_text: 'pop',
-                    question_id: answer.id,
+                    question_id: answer[0].id,
                     points: 2,
                     correct: true,
                   }),
