39da8da21cb5873b9ce4825b1bfc292e5ac4072c
Joe Heitkamp <noetic97@gmail.com>
2017-09-09 14:45:35 -0600

    Add JWT auth to controller

--- router.js
+++ router.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const auth = require('./controllers/authController');
 const teacher = require('./controllers/teacherController');
 const quiz = require('./controllers/quizController');
 
 const router = express.Router();
+
+// Authorization
+router.post('/auth', auth.auth);
 
 // Teacher
 router.get('/teachers/:id/folders', teacher.indexFolders);
 
 // Quiz
 router.get('/quizzes', quiz.index);
-
 
 module.exports = router;
