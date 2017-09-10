bdaa3fdf86bc2d723df47d34f2c27be724d45236
George Goering <ggoeringgg@gmail.com>
2017-09-10 11:48:59 -0600

    add room table to administer quizzes from

---
+++ db/migrations/20170910112432_add_room_table.js
@@ -0,0 +1,12 @@
+
+exports.up = (knex) => {
+  return knex.schema.createTable('room', (table) => {
+    table.string('id').primary();
+    table.integer('quiz_id').unsigned();
+    table.foreign('quiz_id').references('quiz.id');
+  });
+};
+
+exports.down = (knex) => {
+  knex.schema.dropTable('room');
+};
