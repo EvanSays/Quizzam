40f5b018144d11eacab39d6a7ab4094a3e555280
George Goering <ggoeringgg@gmail.com>
2017-09-10 13:38:30 -0600

    add get request endpoint for serving a quiz to a room

--- db/migrations/20170910112432_add_room_table.js
+++ db/migrations/20170910112432_add_room_table.js
@@ -1,12 +1,12 @@
 
 exports.up = (knex) => {
   return knex.schema.createTable('room', (table) => {
-    table.string('id').primary();
+    table.string('id', 4).primary();
     table.integer('quiz_id').unsigned();
     table.foreign('quiz_id').references('quiz.id');
   });
 };
 
 exports.down = (knex) => {
-  knex.schema.dropTable('room');
+  return knex.schema.dropTable('room');
 };
