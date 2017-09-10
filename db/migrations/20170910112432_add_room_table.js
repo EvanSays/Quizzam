658c5d1492e84cecfea721fac58df0dcb89bb159
George Goering <ggoeringgg@gmail.com>
2017-09-10 11:55:43 -0600

    Update 20170910112432_add_room_table.js

--- db/migrations/20170910112432_add_room_table.js
+++ db/migrations/20170910112432_add_room_table.js
@@ -1,7 +1,7 @@
 
 exports.up = (knex) => {
   return knex.schema.createTable('room', (table) => {
-    table.string('id').primary();
+    table.string('id', 4).primary();
     table.integer('quiz_id').unsigned();
     table.foreign('quiz_id').references('quiz.id');
   });
