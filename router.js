6b91f571f6da2030f407b0138debb8860ef324ea
Evan M <evansays+github@evansays.io>
2017-09-12 16:37:17 -0600

    Revert "PATCH /quizzes/:id"

--- router.js
+++ router.js
@@ -3,7 +3,6 @@ const user = require('./controllers/userController');
 const quiz = require('./controllers/quizController');
 const room = require('./controllers/roomController');
 const question = require('./controllers/questionController');
-const answer = require('./controllers/answerController');
 
 const router = express.Router();
 
@@ -16,15 +15,13 @@ router.post('/users/new', user.createUser);
 // Quiz
 router.get('/quizzes', quiz.index);
 router.post('/quizzes', quiz.addQuiz);
-router.patch('/quizzes/:id', quiz.editQuiz);
 
-// Questions
-router.get('/quizzes/:quizId/questions', question.indexQuestions);
-router.post('/quizzes/:quizId/questions', question.addQuestion);
+router.get('/quizzes/:quizId/questions', quiz.indexQuestions);
+router.post('/quizzes/:quizId/questions', quiz.addQuestion);
 
 // Answers
-router.get('/questions/:questionId/answers', answer.indexAnswers);
-router.post('/questions/:questionId/answers', answer.addAnswer);
+router.get('/questions/:questionId/answers', question.indexAnswers);
+router.post('/questions/:questionId/answers', question.addAnswer);
 
 // Room
 router.get('/room/:id', room.quiz);
