82a8847c4ba71b7251b64c36261c0c09fdb0c8dc
Evan Miller <evanmillerny@gmail.com>
2017-09-16 13:18:00 -0600

    add setstate with questions and answer

--- src/components/EditQuiz.js
+++ src/components/EditQuiz.js
@@ -5,34 +5,57 @@ class EditQuiz extends Component {
   constructor() {
     super();
     this.state = {
-
+      isEditing : false,
+      questions : null,
+      answers: []
     };
+    this.toggleEdit = this.toggleEdit.bind(this);
+  }
+
+  componentWillMount() {
+    const { edit, answers } = this.props
+    const answerArray = [];
+    const questionArray = [];
+    const questions = edit.questions;
+    questions.forEach((question) => {
+      questionArray.push({ id: question.id, question_text: question.question_text });
+      question.answers.forEach((quesAns) => {
+        answerArray.push(quesAns);
+      });
+    });
+
+    this.setState({questions: questionArray, answers: answerArray });
+  }
+  
+
+  toggleEdit() {
+    console.log('toggleEdit');
+    
+    
   }
   
   render() {
     const { edit } = this.props;
 
-    const questions = edit.questions.map(question => {
-      console.log('question', question);
-      
+    const questions = edit.questions.map((question) => {
       return (
         <div key={getKey()}>
           <h2>{question.question_text}</h2>
           {
-            question.answers.map((answer, index) => {
-              console.log('answer', answer);
-              
+            question.answers.map((answer) => {
               return (
                 <div key={getKey()}>
-                  <input type="radio"
+                  <input 
+                    type="radio"
                     id={answer.answer_text}
                     name={answer.answer_text}
                     value={answer.answer_text} />
-                  <label htmlFor>{answer.answer_text}</label>
+                  <label htmlFor={answer.answer_text}>{answer.answer_text}</label>
                 </div>
               )
             })
           }
+          <button onClick={this.toggleEdit}>edit question</button>
         </div>
       )
     })
@@ -41,6 +64,7 @@ class EditQuiz extends Component {
       <div>
         <h1>{edit.name}</h1>
         {questions}
+        <button>submit changes</button>
       </div>
     );
   }
