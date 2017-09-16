0b9342ada0cb50a3d80be263defe4ebe37d4e05a
Evan Miller <evanmillerny@gmail.com>
2017-09-15 18:38:18 -0600

    add select quiz data to state to help render in edit quiz

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { string, number } from 'prop-types';
 
-const QuizCard = ({ quizData, postRoom, selectQuiz }) => {
+const QuizCard = ({ quizData, postRoom, editQuiz }) => {
+  
   const questionCount = quizData.questions.length;
   return (
     <section className="quiz-card">
       <p>{quizData.title}</p>
       <p>{questionCount}</p>
-      <button onClick={() => selectQuiz(quizData)} className="quiz-edit-button">Edit Quiz</button>
+      <button onClick={() => editQuiz(quizData)} 
+              className="quiz-edit-button"
+              >Edit Quiz
+      </button>
       <button className="quiz-delete">Delete Quiz</button>
       <button
         onClick={() => postRoom(quizData.id)}
