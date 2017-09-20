1f7dc96c9b0c94a1050ea642a4b108533c9b65d2
Joe Heitkamp <noetic97@gmail.com>
2017-09-20 00:17:17 -0600

    Add DELETE function to contoller

--- controllers/answerController.js
+++ controllers/answerController.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-restricted-syntax */
+
 const { db } = require('../server');
 
 exports.indexAnswers = (req, res) => {
@@ -19,6 +21,11 @@ exports.indexAnswers = (req, res) => {
 
 exports.addAnswer = (req, res) => {
   const newAnswer = req.body;
+  for (const requiredParams of ['answer_text']) {
+    if (!req.body[requiredParams]) {
+      return res.status(422).json({ error: 'You are missing the answer text!' });
+    }
+  }
   return db('answer')
     .insert(newAnswer, 'id')
     .then(answer => res.status(201).json({
@@ -31,12 +38,36 @@ exports.addAnswer = (req, res) => {
 
 exports.editAnswer = (req, res) => {
   const newPatch = req.body;
+  const { answerId } = req.params;
+
   db('answer')
-    .where('id', req.params.id)
+    .where('id', answerId)
     .select()
     .update(newPatch, 'id')
     .then((answer) => {
-      res.status(201).json({ id: answer });
+      if (!answer.length) {
+        return res.status(404).json({ error: `The answer with ID# ${answerId} was not found and could not be updated` });
+      }
+      return res.status(201).json({ id: answer[0] });
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+exports.delAnswer = (req, res) => {
+  const { answerId } = req.params;
+
+  db('question')
+    .where('id', answerId)
+    .del()
+    .returning('*')
+    .then((answer) => {
+      if (!answer.length) {
+        return res.status(404).json({ error: `The answer with ID# ${answerId} was not found and could not be deleted` });
+      }
+      return res.status(200).json({
+        success: `Answer #${answerId} was deleted.`,
+        deletedAnswer: answer,
+      });
     })
     .catch(error => res.status(500).json({ error }));
 };
