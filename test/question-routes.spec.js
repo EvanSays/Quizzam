04276ea888ce57db8258de842cc0a9de40f3ac29
Joe Heitkamp <noetic97@gmail.com>
2017-09-19 23:18:58 -0600

    Add happy path PATCH tests for questions route

--- test/question-routes.spec.js
+++ test/question-routes.spec.js
@@ -140,5 +140,98 @@ describe('Question API routes', () => {
             });
         });
     });
+
+    it('SAD PATH - Should not allow a question to be posted if it\'s missing a required parameter', (done) => {
+      chai.request(app)
+        .get('/api/v1/quizzes')
+        .end((err, res) => {
+          res.status.should.equal(200);
+          const id = res.body[0].id;
+
+          chai.request(app)
+            .post(`/api/v1/quizzes/${id}/questions`)
+            .send({
+              subject: 'lunar studies',
+              question_type: 'multiple choice',
+              difficulty: 10,
+              quiz_id: id,
+            })
+            .end((err1, res1) => {
+              should.exist(err1);
+              should.exist(res1);
+              res1.status.should.equal(422);
+              res1.res.should.have.property('statusMessage');
+              res1.res.statusMessage.should.equal('Unprocessable Entity');
+              res1.body.should.have.property('error');
+              res1.body.error.should.equal('You are missing the question text!');
+              done();
+            });
+        });
+    });
+  });
+
+  describe('PATCH /api/v1/quizzes/:id/questions', () => {
+    it('Should return the questions\'s ID if an existing question is updated', (done) => {
+      chai.request(app)
+        .get('/api/v1/quizzes')
+        .end((err, res) => {
+          const id = res.body[0].id;
+
+          chai.request(app)
+            .get(`/api/v1/quizzes/${id}/questions`)
+            .end((err1, res1) => {
+              const questionId = res1.body[0].id;
+
+              chai.request(app)
+                .patch(`/api/v1/quizzes/${id}/questions/${questionId}`)
+                .send({
+                  subject: 'moon studies',
+                })
+                .end((err2, res2) => {
+                  should.not.exist(err);
+                  res2.status.should.equal(201);
+                  res2.should.be.json; //eslint-disable-line
+                  res2.body.should.be.a('object');
+                  res2.body.should.have.property('id');
+                  res2.body.id.should.be.a('number');
+                  res2.body.id.should.equal(questionId);
+                  done();
+                });
+            });
+        });
+    });
+
+    it('Should update the property of the question that is sent in the request', (done) => {
+      chai.request(app)
+        .get('/api/v1/quizzes')
+        .end((err, res) => {
+          const id = res.body[0].id;
+
+          chai.request(app)
+            .get(`/api/v1/quizzes/${id}/questions`)
+            .end((err1, res1) => {
+              const testQuestion = res1.body.find(array => array.question_text === 'What is your favorite color?');
+              const questionId = testQuestion.id;
+              testQuestion.subject.should.equal('Jquery');
+
+              chai.request(app)
+                .patch(`/api/v1/quizzes/${id}/questions/${questionId}`)
+                .send({
+                  subject: 'moon studies',
+                })
+                .end((err2, res2) => {
+                  res2.status.should.equal(201);
+
+                  chai.request(app)
+                    .get(`/api/v1/quizzes/${id}/questions`)
+                    .end((err3, res3) => {
+                      const testQuestion2 = res3.body.find(array => array.question_text === 'What is your favorite color?');
+                      testQuestion2.subject.should.equal('moon studies');
+                      done();
+                    });
+                });
+            });
+        });
+    });
   });
 });
